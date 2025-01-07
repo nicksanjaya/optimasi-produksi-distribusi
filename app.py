8944c6b59b5e2fc11c37e59d540fdd1877f868ce
nicksanjaya <nicky.sanjaya@gmail.com>
2025-01-07 09:50:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,57 @@ def margin(df):
     df['Margin_2'] = df['Sales'] - df['Cost_Pabrik_2']
     df['Margin_3'] = df['Sales'] - df['Cost_Pabrik_3']
 
+#Optimasi
+def optimization(df,capacity):
+    #Variable
+    model = pyo.ConcreteModel()
+    model.Pro = pyo.Var(range(3), range(len(df.Produk)), domain=pyo.NonNegativeReals)
+    pro = model.Pro
+    
+    #Batasan kapasitas
+    model.limits = pyo.ConstraintList()
+    for pabrik in range(3):
+        for produk in range(len(df.Produk)):
+            model.limits.add(expr = pro[pabrik,produk] <= capacity[pabrik])
 
+    #Batasan Pasar
+    pro_sums = {kolom: sum(pro[pabrik, kolom] for pabrik in range(3)) for kolom in range(len(df.Produk))}
+    model.demand = pyo.ConstraintList()
+    for kolom, pro_sum in pro_sums.items():
+        model.demand.add(expr = pro_sum <= sum(df[f'Demand_Area_{i}'][kolom] for i in range(1, 5)))
+        
+    # Objektif
+    Margin_1 = df['Margin_1'].tolist()
+    Margin_2 = df['Margin_2'].tolist()
+    Margin_3 = df['Margin_3'].tolist()
+
+    koefisien = [
+        Margin_1,  # Koefisien untuk pabrik 0
+        Margin_2,  # Koefisien untuk pabrik 1
+        Margin_3   # Koefisien untuk pabrik 2
+    ]
+    
+    pro_sum_obj = sum(koefisien[pabrik][kolom] * pro[pabrik, kolom] for pabrik in range(3) for kolom in range(len(df.Produk)))
+    model.obj = pyo.Objective(expr = pro_sum_obj, sense = maximize)
+
+    #Solver
+    opt = SolverFactory('glpk')
+    results = opt.solve(model, tee=True) # tee=True untuk menampilkan output solver di konsol
+    
+    #Cek hasil solusi
+    if results.solver.status != SolverStatus.ok or results.solver.termination_condition != TerminationCondition.optimal:
+        st.error(f"Solusi tidak ditemukan! Status solver: {results.solver.status}, Termination condition: {results.solver.termination_condition}")
+        return
+    
+    # Menambahkan garis pembatas
+    st.markdown('---'*10)
+    
+    #Hasil optimasi
+    for pabrik in range (3):
+        st.write(f"Produksi Pabrik {pabrik+1}")
+        for produk in range(len(df.Produk)):
+            st.write(f'<center><b><h3>Produk {df.Produk[produk]} = {pyo.value(pro[pabrik,produk]):,.0f} pcs</b></h3>', unsafe_allow_html=True)
+    
 #Upload File 
 uploaded_file = st.file_uploader("Upload Excel Master Data", type=["xlsx"])
 
@@ -68,5 +118,11 @@ if uploaded_file is not None:
     capacity_1 = st.number_input("Kapasitas Pabrik 1:", min_value=0)
     capacity_2 = st.number_input("Kapasitas Pabrik 2:", min_value=0)
     capacity_3 = st.number_input("Kapasitas Pabrik 3:", min_value=0)
-
+    capacity = [capacity_1, capacity_2, capacity_3]
+    
+    if st.button("Calculate"):
+        try:
+            optimization(df, capacity)
+        except Exception as e:
+            st.error(f"Error : {e}")
 
